b9b21e332ea305a5013285ea53b6a3db2a91c168
David <daniel.david.surla@gmail.com>
2024-02-29 13:34:32 +1100

    Create CqliConnector.java

---
+++ src/main/java/dreamhouse/conchord/services/CqliConnector.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CqliConnector
+{
+  public static void start(String[] args)
+  {
+    try
+    (
+      Connection connection = DriverManager.getConnection("jdbc:sqlite:consumers.db");
+      Statement statement = connection.createStatement();
+    )
+    {
+      statement.setQueryTimeout(60); // 60 seconds synchronous connection
+
+      // explain keyword to prevent unwanted commits
+      statement.executeUpdate("drop table if exists consumer");
+      ResultSet rs = statement.executeQuery("explain select * from consumer");
+
+      while(rs.next())
+      {
+        System.out.println("id = " + rs.getString("id"));
+      }
+    }
+    catch (SQLException sqlException)
+    {
+      sqlException.printStackTrace(System.err);
+    }
+  }
+}
